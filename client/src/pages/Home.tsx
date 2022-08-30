93e9c8014f2635814150831e17aa0f0d0291df69
davide <davidebellucci975@gmail.com>
2022-08-30 16:37:12 +0200

    add parent component to pass the same socket instance to both inboxes page and single chat page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,52 +1,17 @@
-import { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import styled from 'styled-components'
-import Loader from '../components/Loader'
-import useLogout from '../features/authentication/hooks/useLogout'
-import useRefreshToken from '../features/authentication/hooks/useRefreshToken'
-import InboxContainer from '../features/chats/components/InboxContainer'
 import { useSocket } from '../features/chats/hooks/useSocket'
-import useFetchData from '../hooks/useFetchData'
-import { INBOXES_URL } from '../utils/urls'
+import Inboxes from './Inboxes'
+import SingleChat from './SingleChat'
 
 const Home = () => {
-  const { refresh } = useRefreshToken()
-  const { logout } = useLogout()
   const socket = useSocket()
 
-  const { data, loading } = useFetchData([INBOXES_URL])
-
-  useEffect(() => {
-    socket?.on('message', (data: any) => {
-      console.log({ data })
-      /*  console.log({ from }) */
-    })
-  }, [socket])
-
   return (
-    <Container>
-      <div>Home</div>
-      <button onClick={refresh}>refresh</button>
-      <button onClick={logout}>logout</button>
-
-      <ChatsWrapper>
-        {loading ? <Loader /> : <InboxContainer inboxes={data[0]} />}
-      </ChatsWrapper>
-    </Container>
+    <Routes>
+      <Route path="/" element={<Inboxes socket={socket} />} />
+      <Route path="/chat/:id" element={<SingleChat socket={socket} />} />
+    </Routes>
   )
 }
 
 export default Home
-
-const Container = styled.div`
-  height: 90vh;
-  border: 2px solid #fff;
-`
-
-const ChatsWrapper = styled.main`
-  width: 100%;
-  height: 90%;
-  display: flex;
-  justify-content: center;
-  border: 2px solid red;
-`
